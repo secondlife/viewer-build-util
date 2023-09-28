296e4650f8f1d0344ff0b4d3773e9f976c9f2cfa
Nat Goodspeed <nat@lindenlab.com>
2023-09-28 09:19:33 -0400

    SL-19242: Remove notarytool --asc-provider switch.
    This was replaced by --team-id, which is optional.

--- sign-pkg-mac/sign.sh
+++ sign-pkg-mac/sign.sh
@@ -114,7 +114,7 @@ then
 fi
 trap "rm '$zip_file'" EXIT
 
-credentials=(--apple-id "$note_user" --password "$note_pass" --asc-provider "$note_asc")
+credentials=(--apple-id "$note_user" --password "$note_pass")
 
 # Here we send the notarization request to Apple's Notarization service,
 # waiting for the result. This typically takes a few seconds inside a CI
